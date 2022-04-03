74c34e88ff27b0be2aee58d64f6dd0734391e735
Gabriel Gamil <gamil.gabriel@gmail.com>
2022-04-03 20:34:55 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import useFetch from "use-http";
+
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState, useCallback } from "react";
 
 function App() {
+  const options = {};
+  const { get, loading, response, error } = useFetch(
+    "http://localhost:8000",
+    options
+  );
+
+  const handleFetchData = useCallback(async () => {
+    const time = await get("/time");
+    console.log("Refresh", response.ok, time);
+    if (response.ok) setTime(time);
+  }, [get, response]);
+
+  const [time, setTime] = useState("");
+  useEffect(() => {
+    handleFetchData();
+  }, [handleFetchData]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {time && <p>{time}</p>}
       </header>
     </div>
   );
